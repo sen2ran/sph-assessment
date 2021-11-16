cbb7142062f079ab649e3a38ff469a4bce38355b
Senthuran <senthu1993@gmail.com>
2021-11-16 15:53:47 +0530

    config file updated

--- frontend/src/api/commonService.ts
+++ frontend/src/api/commonService.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-import endpoint from "./endpoint"
+import config from "./endpoint"
 import { useAsyncAwait } from "../hooks/asyncAwaitHook";
 
 const instance = axios.create({
-  baseURL: endpoint.baseUrl,
+  baseURL: config.baseUrl,
 });
 
 export const GetDetailsById = async () =>
-  useAsyncAwait(instance.get(endpoint.api.getDetails));
+  useAsyncAwait(instance.get(config.api.getDetails));
--- frontend/src/api/endpoint.ts
+++ frontend/src/api/endpoint.ts
@@ -1,9 +1,10 @@
-const endpoint = {
+const config = {
   baseUrl: "http://localhost:3001/api",
   api: {
     getSearchValue: "/search",
     getDetails: "/search/details/123",
   },
+  inputDebounceInterval: 500,
 };
 
-export default endpoint;
+export default config;
