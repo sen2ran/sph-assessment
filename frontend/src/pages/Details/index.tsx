761a3c6fb2730f8e62f1673051e2b153daa1685b
Senthuran <senthu1993@gmail.com>
2021-11-17 08:08:12 +0530

    Details page updated

--- frontend/src/pages/Details/index.tsx
+++ frontend/src/pages/Details/index.tsx
@@ -1,3 +1,70 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Card, Avatar, Row } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+import { GetDetailsById } from "../../api/commonService";
+
+const { Meta } = Card;
+
 export default () => {
-  return <h1>Detail</h1>;
+  const { id } = useParams<any>();
+  useEffect(() => {
+    const getDetails = async () => {
+      const [listData, listErr] = await GetDetailsById();
+      if (listErr) {
+        console.log(listErr);
+      }
+      console.log(listData.data.data);
+    };
+
+    getDetails();
+  }, [id]);
+
+  return (
+    <div style={{ paddingBottom: "200px" }}>
+      <Row justify="center" align="middle">
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <img
+              alt="example"
+              style={{  }}
+              src="https://source.unsplash.com/random?h=200"
+            />
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            title="Card title"
+            description={
+              <>
+                <p>
+                  Author : <b>Senthurna</b>
+                </p>
+                <p>
+                  Author : <b>Senthurna</b>
+                </p>
+                <p>
+                  Author : <b>Senthurna</b>
+                </p>
+                <p>
+                  Author : <b>Senthurna</b>
+                </p>
+              </>
+            }
+          />
+        </Card>
+      </Row>
+    </div>
+  );
 };
