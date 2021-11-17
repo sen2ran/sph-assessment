18e16ad20846630540e75e1a018a7240c92e7790
Senthuran <senthu1993@gmail.com>
2021-11-17 13:57:31 +0530

    Dummy data

---
+++ backend/assets/dummyData.js
@@ -0,0 +1,30 @@
+const searchDummyData = [
+    {
+        title: "Ant Design Title 1",
+        id: 9999,
+    },
+    {
+        title: "Ant Design Title 2",
+        id: 9999,
+    },
+    {
+        title: "Ant Design Title 3",
+        id: 9999,
+    },
+    {
+        title: "Ant Design Title 4",
+        id: 9999,
+    },
+];
+
+const detailDummyData = {
+    author: "demo Administrator",
+    title: "Test",
+    content: "This is a test",
+    edited: "2010-06-23T09:09:50.654Z",
+    created: "2010-06-22T10:22:20.000Z",
+}
+module.exports = {
+    searchDummyData: searchDummyData,
+    detailDummyData: detailDummyData
+};
