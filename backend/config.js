c5766a95b64b7b9751e7aff0be065ab8802611f9
Senthuran <senthu1993@gmail.com>
2021-11-17 13:57:22 +0530

    axios instance added

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,16 @@
 const dotenv = require("dotenv");
+const axios = require("axios");
 dotenv.config();
+
+let instance = axios.create({
+  baseURL: process.env.HOST_ADDRESS,
+  auth: {
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+  },
+});
+
 module.exports = {
   port: process.env.PORT,
+  instance: instance
 };
